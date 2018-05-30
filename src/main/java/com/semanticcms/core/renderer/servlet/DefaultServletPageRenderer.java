4557cd7a2b5dd5772a5c21061bbfb97dfb30fed4
AO Industries, Inc. <support@aoindustries.com>
2018-05-30 18:34:34 -0500

    Renderer interface defined.

---
+++ src/main/java/com/semanticcms/core/renderer/servlet/DefaultServletPageRenderer.java
@@ -0,0 +1,63 @@
+/*
+ * semanticcms-core-renderer-servlet - Support for SemanticCMS pages rendered in a Servlet environment.
+ * Copyright (C) 2018  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-renderer-servlet.
+ *
+ * semanticcms-core-renderer-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-renderer-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-renderer-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.renderer.servlet;
+
+import com.semanticcms.core.model.Page;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class DefaultServletPageRenderer implements ServletPageRenderer {
+
+	private final Page page;
+	private final Map<String,? extends Object> attributes;
+
+	public DefaultServletPageRenderer(Page page, Map<String,? extends Object> attributes) {
+		this.page = page;
+		this.attributes = attributes;
+	}
+
+	@Override
+	public void doRenderer(Writer out) throws IOException {
+		try {
+			HttpServletRequest request = (HttpServletRequest)attributes.get(REQUEST_RENDERER_ATTRIBUTE);
+			if(request == null) throw new ServletException("Page attribute not found: " + REQUEST_RENDERER_ATTRIBUTE);
+			HttpServletResponse response = (HttpServletResponse)attributes.get(RESPONSE_RENDERER_ATTRIBUTE);
+			if(response == null) throw new ServletException("Page attribute not found: " + RESPONSE_RENDERER_ATTRIBUTE);
+			doRenderer(page, request, response, out);
+		} catch(ServletException e) {
+			throw new IOException(e);
+		}
+	}
+
+	@Override
+	public abstract void doRenderer(
+		Page page,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Writer out
+	) throws IOException, ServletException;
+}
---
+++ src/main/java/com/semanticcms/core/renderer/servlet/ServletPageRenderer.java
@@ -0,0 +1,45 @@
+/*
+ * semanticcms-core-renderer-servlet - Support for SemanticCMS pages rendered in a Servlet environment.
+ * Copyright (C) 2018  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-renderer-servlet.
+ *
+ * semanticcms-core-renderer-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-renderer-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-renderer-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.renderer.servlet;
+
+import com.semanticcms.core.model.Page;
+import com.semanticcms.core.renderer.PageRenderer;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ServletPageRenderer extends PageRenderer {
+
+	String REQUEST_RENDERER_ATTRIBUTE = ServletPageRenderer.class.getName() + ".request";
+	String RESPONSE_RENDERER_ATTRIBUTE = ServletPageRenderer.class.getName() + ".response";
+
+	// TODO: Java 1.8: default methods instead of separate DefaultServletRenderer class
+	void doRenderer(
+		Page page,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		Writer out
+	) throws IOException, ServletException;
+}
